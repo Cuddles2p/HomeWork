6d856a1d008e2b6f15a9f42343e788e054278aa0
Stas <stask99mail@gmail.com>
2021-11-09 20:57:37 +0500

    upload_dz_1

---
+++ Кузьмин_Стас_dz_1/task_3.py
@@ -0,0 +1,12 @@
+# Склонение процента
+for i in range(0,100):
+    new_str = str(i+1)
+    new_list = list (new_str)
+    if int(new_list[-1])== 1 and i+1!=11:
+        print('{} процент'.format(i+1))
+    elif int(new_list[-1])>1 and int(new_list[-1])<=4:
+        print('{} процента'.format(i+1))
+        if i+1>10 and i+1<=14:
+            print('{} процентов'.format(i+1))
+    else:
+        print('{} процентов'.format(i+1))
