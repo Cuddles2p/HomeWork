51db9bb54f995225dab3bf9d8cbbf31cbf21c1c2
Stas <stask99mail@gmail.com>
2021-11-12 20:41:05 +0500

    upload file

--- Кузьмин_Стас_dz_1/task_2.py
+++ Кузьмин_Стас_dz_1/task_2.py
@@ -1,30 +1,67 @@
-# Задание a:
-list_cub = [i**3 for i in range(1, 1001, 2)]
+# Вариант заданиа а
+list_cub = []
 
-def sum_cub(number):
-    num = 0
+for i in range (1,1001):
+    if (i % 2!=0):
+        i=i**3
+        list_cub.append(i)
 
-    while number != 0:
-        num += number % 10
-        number //= 10
+numbers_sum = 0
+numbers_list =[]
+for i in range(len(list_cub)):
+    my_str = str(list_cub[i])
+    my_list = list (my_str)
 
-    return num
-result = sum(filter(lambda num: sum_cub(num) % 7 == 0, list_cub))
-res2 = sum(filter(lambda num: sum_cub(num + 17) % 7 == 0, list_cub))
+    for i in range(len(my_list)):
+        my_list[i] = int(my_list[i])
+    for i in range(len(my_list)):
+        numbers_sum = numbers_sum + my_list[i]
+    if numbers_sum % 7 == 0:
 
-print(result)
+        numbers_list.append(numbers_sum)
+print(numbers_list)
 
-#Задание b:
-list_cub = [i**3 for i in range(1, 1001, 2)]
+# Вариант заданиа b
+list_cub = []
 
-def sum_cub(number):
-    num = 0
+for i in range (1,1001):
+    if (i % 2!=0):
+        i=i**3+17
+        list_cub.append(i)
 
-    while number != 0:
-        num += number % 10
-        number //= 10
+numbers_sum = 0
+numbers_list =[]
+for i in range(len(list_cub)):
+    my_str = str(list_cub[i])
+    my_list = list (my_str)
 
-    return num
-result = sum(filter(lambda num: sum_cub(num + 17) % 7 == 0, list_cub))
+    for i in range(len(my_list)):
+        my_list[i] = int(my_list[i])
+    for i in range(len(my_list)):
+        numbers_sum = numbers_sum + my_list[i]
+    if numbers_sum % 7 == 0:
+        numbers_list.append(numbers_sum)
+print(numbers_list)
 
-print(result)
+# Вариант заданиа c
+list_cub = []
+
+for i in range (1,1001):
+    if (i % 2!=0):
+        i=i**3+17
+        list_cub.append(i)
+
+numbers_sum = 0
+
+for i in range(len(list_cub)):
+    my_str = str(list_cub[i])
+    list_cub.clear
+    my_list = list (my_str)
+
+    for i in range(len(my_list)):
+        my_list[i] = int(my_list[i])
+    for i in range(len(my_list)):
+        numbers_sum = numbers_sum + my_list[i]
+    if numbers_sum % 7 == 0:
+        numbers_list.append(numbers_sum)
+print(numbers_list)
