5b455d7e4fe5fc16694fcc3d6720004853a16f4f
Stas <stask99mail@gmail.com>
2021-11-12 20:12:14 +0500

    upload Home Work dz 2

---
+++ Кузьмин_Стас_dz_2/task_1.py
@@ -0,0 +1,14 @@
+# Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+multiplication = 15 * 3
+print(type(multiplication))
+division = 15 / 3
+print(type(division))
+integer_division = 15 // 2
+print(type(integer_division))
+exponentiation = 15 ** 2
+print(type(exponentiation))
