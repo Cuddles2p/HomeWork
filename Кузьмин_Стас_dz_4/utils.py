43394f672dc3872306a48e1cbcf31aff6786af8b
Stas <stask99mail@gmail.com>
2021-11-21 23:27:12 +0500

    upload DZ in Lesson_4

---
+++ Кузьмин_Стас_dz_4/utils.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime
+
+response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+content = response.content.decode(encoding=response.encoding)
+
+
+def currency_rates(char):
+    my_list1 = []
+    my_list2 = []
+    my_time = []
+    for el in content.split('CharCode>')[1:]:
+        code = el.split('<')[0]
+        my_list1.append(code)
+    for el in content.split('Value>')[1:]:
+        numb = el.split('<')[0]
+        my_list2.append(numb)
+
+    result = dict(zip(my_list1, my_list2))
+    numbs = char
+    print(f'{numbs} = {result[numbs]} руб')
+
+    return result[numbs]
+
+
+if __name__ == '__main__':
+    print(("hi!"))
+    print(currency_rates('USD'))
+    print(currency_rates('EUR'))
