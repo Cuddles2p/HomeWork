725253a126031dc0236e9497949bcae0ed48074d
Stas <stask99mail@gmail.com>
2021-11-30 15:45:47 +0500

    Upload Dz_6

--- Кузьмин_Стас_dz_5/task_5_2.py
+++
@@ -1,8 +0,0 @@
-# *(вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
-
-MAX_NUM = 10 ** 4
-
-gen = {num for i in range(1, MAX_NUM + 1) for num in range(1, MAX_NUM + 1) if num % 2 != 0}
-
-print(gen)
-print(type(gen))
